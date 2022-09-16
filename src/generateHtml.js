7be6cc2ec9697472e102048b46caa656c801b471
aleja <92136921+mariather@users.noreply.github.com>
2022-09-16 16:03:48 -0700

    create html and target imports

---
+++ src/generateHtml.js
@@ -0,0 +1,29 @@
+function generateHtml(allTeamCardsStr) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="./assets/css/reset.css">
+        <link rel="stylesheet" href="./assets/css/style.css">
+        <link rel="shortcut icon" href="./assets/img/favicon.ico?" type="image/x-icon">
+        <title>Team Profiles</title>
+    </head>
+    <body>
+        <header>
+            <h1>My Team</h1>
+        </header>
+        <section>
+            ${allTeamCardsStr}
+            <!--dynamic content goes here-->
+        </section>
+       
+    </body>
+    </html>
+    `
+}
+
+module.exports = generateHtml;
